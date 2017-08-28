44482d55dc79599daba51f3cbc8564a8e9175bd1
YDJSZY <260211963@qq.com>
2017-08-28 22:10:36 +0800

    commit

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
  */
 const React = require("react");
 const ReactDom = require("react-dom");
+<<<<<<< Updated upstream
 import "whatwg-fetch";
 require("./route/nav");
 require("./route/routes");
@@ -29,3 +30,140 @@ getMyInfo();
 console.log(12)
 
 
+=======
+var arr = [
+    <h1>Hello world!</h1>,
+    <h2>React is awesome</h2>,
+];
+ReactDom.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('main')
+);
+
+ReactDom.render(
+    <div>{arr}</div>,
+    document.getElementById('main1')
+);
+
+var HelloMessage = React.createClass({
+    render: function() {
+        return <h1>Hello {this.props.name}</h1>;
+    }
+});
+
+ReactDom.render(
+    <HelloMessage name="John" />,
+    document.getElementById('main2')
+);
+
+var Demoa = React.createClass({
+    render: function () {
+        return <a>{this.props.title}</a>
+    }
+})
+var NotesList = React.createClass({
+    render: function() {
+        return (
+            <ol>
+                {
+                    React.Children.map(this.props.children, function (child) {
+                        return <li>{child}<Demoa title="fuck"></Demoa></li>;
+                    })
+                }
+            </ol>
+        );
+    }
+});
+
+ReactDom.render(
+    <NotesList>
+        <span>hello</span>
+        <span>world</span>
+    </NotesList>,
+    document.getElementById("main3")
+);
+
+var MyTitle = React.createClass({
+    propTypes: {
+        title: React.PropTypes.string.isRequired,
+    },/*定义属性类型*/
+
+    getDefaultProps : function () {
+        return {
+            fuck : 'Hello World'
+        };
+    },/*定义属性默认值*/
+
+    render: function() {
+        return <h1> {this.props.title},{this.props.fuck} </h1>;
+    }
+});
+
+ReactDom.render(
+    <MyTitle title={"fuck"}>
+    </MyTitle>,
+    document.getElementById("main4")
+);
+
+var MyComponent = React.createClass({
+    handleClick: function() {
+        console.log(this.refs.myTextInput)
+        this.refs.myTextInput.focus();
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="text" ref="myTextInput" />
+                <input type="button" value="Focus the text input" onClick={this.handleClick} />
+            </div>
+        );
+    }
+});
+
+ReactDom.render(
+    <MyComponent />,
+    document.getElementById('main5')
+);
+
+var LikeButton = React.createClass({
+    getInitialState: function() {
+        return {liked: false};
+    },/*定义初始状态*/
+    handleClick: function(event) {
+        this.setState({liked: !this.state.liked});
+    },
+    render: function() {
+        var text = this.state.liked ? 'like' : 'haven\'t liked';
+        return (
+            <p onClick={this.handleClick}>
+                You {text} this. Click to toggle.
+            </p>
+        );
+    }
+});
+
+ReactDom.render(
+    <LikeButton />,
+    document.getElementById('main6')
+);
+
+var Input = React.createClass({
+    getInitialState: function() {
+        return {value: 'Hello!'};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    render: function () {
+        var value = this.state.value;
+        return (
+            <div>
+                <input type="text" value={value} onChange={this.handleChange} />
+                <p>{value}</p>
+            </div>
+        );
+    }
+});
+
+ReactDom.render(<Input/>, document.getElementById('main7'));
+>>>>>>> Stashed changes
---
+++ assets/static/0.101a1f696f83011c081a.hot-update.js
@@ -0,0 +1,43 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(module) {/* REACT HOT LOADER */ if (true) { (function () { var ReactHotAPI = __webpack_require__(2), RootInstanceProvider = __webpack_require__(10), ReactMount = __webpack_require__(12), React = __webpack_require__(98); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
+
+	/**
+	 * Created by luwenwei on 17/2/5.
+	 */
+	const React = __webpack_require__(98);
+	const ReactDom = __webpack_require__(99);
+	var arr = [React.createElement("h1", null, "Hello world!"), React.createElement("h2", null, "React is awesome")];
+	ReactDom.render(React.createElement("h1", null, "Hello, world!"), document.getElementById('main'));
+
+	ReactDom.render(React.createElement("div", null, arr), document.getElementById('main1'));
+
+	var HelloMessage = React.createClass({
+	    displayName: "HelloMessage",
+
+	    render: function () {
+	        return React.createElement("h1", null, "Hello ", this.props.name);
+	    }
+	});
+
+	ReactDom.render(React.createElement(HelloMessage, { name: "John" }), document.getElementById('main2'));
+
+	var NotesList = React.createClass({
+	    displayName: "NotesList",
+
+	    render: function () {
+	        return React.createElement("ol", null, React.Children.map(this.props.children, function (child) {
+	            return React.createElement("li", null, child);
+	        }));
+	    }
+	});
+
+	ReactDom.render(React.createElement(NotesList, null, React.createElement("span", null, "hello"), React.createElement("span", null, "world")), document.getElementById("main3"));
+
+	/* REACT HOT LOADER */ }).call(this); } finally { if (true) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = __webpack_require__(189); if (makeExportsHot(module, __webpack_require__(98))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot apply hot update to " + "app.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)(module)))
+
+/***/ }
+])
