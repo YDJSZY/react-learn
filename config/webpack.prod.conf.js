b2579a9f7cbc5caf734a59c7c7ea674696ae842d
YDJSZY <260211963@qq.com>
2017-09-14 18:17:57 +0800

    commit

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -3,18 +3,32 @@
  */
 var webpack = require('webpack');
 var path = require('path');
-var plugins = require("../plugins");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var config = {
-    //入口文件输出配置
     output: {
-        path: path.resolve(__dirname, 'dist/'),
+        path: path.resolve(__dirname, '../dist/'),
         publicPath: './',
         filename: '[name].js',
         chunkFilename: '[name].[chunkhash:5].js',
-    }
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({names: ['vendor', 'manifest'],filename:'[name].[chunkhash:5].js',minChunks: Infinity,children: true }),
+        new ExtractTextPlugin('[name].[chunkhash:5].css'),
+        new webpack.optimize.UglifyJsPlugin({
+            // Don't beautify output (enable for neater output)  
+            beautify: false,
+            // Eliminate comments  
+            comments: false,
+            compress: {
+                warnings: false,
+                // Drop `console` statements  
+                drop_console: true
+            }
+        })
+    ]
 }
 
 module.exports = config;
